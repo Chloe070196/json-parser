16fe720bfbf9b01dadc1df36eb62f5167d1e4cf1
Chloe Zermatten <chloezermatten@gmail.com>
2024-04-08 11:02:58 +0100

    feat: treat user input as file name

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const parse = require("./lib/parser.js");
 const { log } = require("./lib/logger.js");
 const exit = require("./lib/exit.js")
 const isValid = require("./lib/inputChecker.js");
+const fs = require('fs')
 
 process.stdin.on("data", (data) => {
-  const dataStr = data.toString().slice(0, data.length - 2)
-
+  const fileName = data.toString().slice(0, data.length - 2)
+  const dataStr = fs.readFileSync(fileName, "utf8");
+  
   if (!isValid(dataStr)) {
     exit("error", 1)
     return;
