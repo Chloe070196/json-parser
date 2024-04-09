6abb208aab23b8749cd99efef4ab18aa699407bb
Chloe Zermatten <chloezermatten@gmail.com>
2024-04-09 11:06:04 +0100

    refactor: extract formatFileName() and getFileContent

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
+const { formatFileName, getFileContent } = require("./lib/utils.js");
+const isValid = require("./lib/inputChecker.js");
 const parse = require("./lib/parser.js");
 const { log } = require("./lib/logger.js");
-const exit = require("./lib/exit.js")
-const isValid = require("./lib/inputChecker.js");
-const fs = require('fs')
+const exit = require("./lib/exit.js");
+const tokenize = require("./lib/lexer.js");
 
 process.stdin.on("data", (data) => {
-  const fileName = data.toString().slice(0, data.length - 2)
-  const dataStr = fs.readFileSync(fileName, "utf8");
-  
+  const fileName = formatFileName(data)
+  const dataStr = getFileContent(fileName)
+
   if (!isValid(dataStr)) {
     exit("error", 1)
     return;
   }
-
-  const parsedJson = parse(dataStr);
+  const tokens = tokenize(dataStr)
+  const parsedJson = parse(tokens);
   log(parsedJson);
 
   exit("success", 0);
